99aaeaa5c01fc582d6fdc5e86fe4edd6e31bcd37
Ning Sun <ningsunprc@gmail.com>
2019-08-27 19:24:58 +0800

    code modification

--- src/PID.cpp
+++ src/PID.cpp
@@ -18,7 +18,7 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   p_error = 0;
   i_error = 0;
   d_error = 0;
-  counter = 0;
+  //counter = 0;
 
 }
 
@@ -27,11 +27,11 @@ void PID::UpdateError(double cte) {
   d_error = cte - p_error;
   p_error = cte;
 
-  if (counter % 15 == 0){
-    i_error /= 5;
-  }
+  // if (counter % 15 == 0){
+  //   i_error /= 5;
+  // }
 
-  counter += 1;
+  // counter += 1;
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -83,22 +83,26 @@ int main() {
 
           pid_throttle.UpdateError(cte * cte - 0.25);
           //normal driving, exluding bend
-          if (speed > 5.0 && speed < 12.0 && abs(cte) < 2.0 && abs(cte_delta) < 0.3)
+          if (speed > 5.0 && speed < 12.0 && abs(cte) < 1.5 && abs(cte_delta) < 0.3)
           {
             throttle_value += pid_throttle.TotalError();
           }
           //when at a bend
-          else if (abs(cte_delta) > 0.3 || abs(cte) > 2.0){
+          else if (abs(cte_delta) > 0.3 || abs(cte) > 1.5){
             //first reduce speed
             if (speed > 4.0) {throttle_value = -0.15;}
             //then slowly move out
-            else {throttle_value = 0.02;}
+            else {throttle_value = 0.1;}
             steer_value *= 2.0; //quickly fix
           }
           //speed limit
-          else if (speed > 12.0) { throttle_value = 0.01; }
+          else if (speed > 12.0) { throttle_value = 0.01;}
           //low speed throttle setting
-          else {throttle_value = 0.1;}
+          else {
+            throttle_value = 0.1;
+            if (speed < 1.0){throttle_value *= 3;}
+            else if (speed < 2.0){throttle_value *= 2;}
+          }
 
           cte_delta = cte - cte_prev;
           cte_prev = cte;
